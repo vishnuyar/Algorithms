3baf62739ec2c11111411256768817a73d666813
vishnuyar <vardhanvishnu@gmail.com>
2020-02-20 00:57:04 +0530

    recipe complete

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,7 +3,13 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  recipe_times = []
+  if(recipe.keys()==ingredients.keys()):
+    for item in recipe:
+      recipe_times.append(ingredients[item]//recipe[item])
+    return min(recipe_times)
+  else:
+    return 0
 
 
 if __name__ == '__main__':
@@ -11,4 +17,5 @@ if __name__ == '__main__':
   # your implementation with different inputs
   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  #recipe_batches(recipe, ingredients)
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
